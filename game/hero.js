5c9bd1c765a56bfa6a45896f20a064d6f3e05832
Tyler Benziger <tabenziger@ucdavis.edu>
2013-03-19 07:28:18 -0700

    Added man-right sprite - change sprites based on direction

--- game/hero.js
+++ game/hero.js
@@ -1,17 +1,22 @@
 /* vim: set tabstop=4 softtabstop=4 shiftwidth=4 expandtab: */
 
+var MAN_RIGHT = 0,
+    MAN_LEFT = 1;
+
 Game.Entity.Hero = Game.Entity.extend({
     type: 'Hero',
     jumpHeight: 54,
     move: {
         'right': function() {
             var collisions = this.hasCollisionWith( 'Terrain.Land' );
+            this.activeSprite = MAN_RIGHT;
             if ( !collisions || !collisions.rightEdge ) {
                 this.pos.x += Game.unit;
             }
         },
         'left': function() {
             var collisions = this.hasCollisionWith( 'Terrain.Land' );
+            this.activeSprite = MAN_LEFT;
             if ( !collisions || !collisions.leftEdge ) {
                 this.pos.x -= Game.unit;
             }
@@ -71,6 +76,17 @@ Game.Entity.Hero.Man = Game.Entity.Hero.extend({
             [ "#00ff00", "#00ff00", "#00ff00", "transparent", "transparent", "transparent", "#00ff00", "transparent", "transparent" ],
             [ "#00ff00", "#00ff00", "transparent", "transparent", "transparent", "transparent", "#00ff00", "#00ff00", "transparent" ],
             [ "#00ff00", "transparent", "transparent", "transparent", "transparent", "transparent", "#00ff00", "#00ff00", "#00ff00"  ]
+        ],
+        [
+            [ "transparent", "transparent", "transparent", "transparent", "transparent", "transparent", "transparent", "transparent", "transparent" ],
+            [ "transparent", "transparent", "transparent", "transparent", "#00ff00", "transparent", "transparent", "transparent", "transparent" ],
+            [ "transparent", "#00ff00", "transparent", "transparent", "transparent", "transparent", "transparent", "transparent", "transparent" ],
+            [ "transparent", "#00ff00", "#00ff00", "#00ff00", "#00ff00", "#00ff00", "#00ff00", "#00ff00", "transparent" ],
+            [ "transparent", "transparent", "transparent", "#00ff00", "#00ff00", "#00ff00", "transparent", "#00ff00", "transparent" ],
+            [ "transparent", "transparent", "transparent", "#00ff00", "#00ff00", "#00ff00", "transparent", "transparent", "transparent" ],
+            [ "transparent", "transparent", "#00ff00", "transparent", "transparent", "transparent", "#00ff00", "#00ff00", "#00ff00" ],
+            [ "transparent", "#00ff00", "#00ff00", "transparent", "transparent", "transparent", "transparent", "#00ff00", "#00ff00" ],
+            [ "#00ff00", "#00ff00", "#00ff00", "transparent", "transparent", "transparent", "transparent", "transparent", "#00ff00"  ]
         ]
     ]
 });
